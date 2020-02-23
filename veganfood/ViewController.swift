5b57efc562d388a61e8a98402253fb27fb9ccf02
Renan Queiroz <renanqueiroz@renans-mac.local>
2020-02-22 21:56:21 -0800

    Initial Commit

---
+++ veganfood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  veganfood
+//
+//  Created by Renan Queiroz on 22/02/20.
+//  Copyright © 2020 devmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
